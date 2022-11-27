2f90e10f917a5ea6f3a14579634bd98d3e9f0258
ivyannaroselyn <109594215+ivyannaroselyn@users.noreply.github.com>
2022-11-27 23:03:44 +0700

    Add files via upload

--- js/x.js
+++ js/x.js
@@ -1,6 +1,6 @@
 images = [
 "16.jpg",
-"11.jpg",
+"12.jpg",
 "19.jpeg",
 "18.jpg",
 "17.jpg",
@@ -8,8 +8,8 @@ images = [
 "15.jpg",
 "14.jpg",
 "13.jpeg",
-"20.jpeg",
-"12.jpg",
+"21.jpeg",
+"11.jpg",
 "06.jpg",
 "08.jpg",
 "09.jpg",
@@ -19,11 +19,6 @@ images = [
 "05.jpg",
 "03.jpg",
 "01.jpeg",
-
-
-
-
-
 ]
 descriptions = [
 "first desription",
@@ -33,12 +28,14 @@ descriptions = [
 var element = document.getElementById("modellingrow")
 console.log(element)
 for (let i = 0; i < images.length; i++) {
+    let idx = i+1
     console.log(images[i])
+    if (i %3 ==0)element.innerHTML+= `<div class="row">`;
     element.innerHTML+=text =  `
-    <div class="col-md-4 col-sm-6">
+    <div class="col-md-4 col-sm-12 border">
                                 <div class="item">
                                     <div class="thumb">
-                                        <a href="img/${images[i]}" data-lightbox="image-1"><div class="hover-effect">
+                                        <a href="img/${images[i]}" data-lightbox="image-1"><div class="hover-effect max">
                                             <div class="hover-content">
                                                 <h2>Number ${i}</h2>
                                                 <p>${descriptions[i]}</p>
@@ -49,6 +46,8 @@ for (let i = 0; i < images.length; i++) {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+    </div>
     `;
+    if (i %3 ==0)element.innerHTML+= `</div>`;
   }
+  
