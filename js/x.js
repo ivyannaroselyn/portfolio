551167672f0abdebdccbe1c0635bf7c1958e7523
ivyannaroselyn <109594215+ivyannaroselyn@users.noreply.github.com>
2022-12-15 22:29:16 +1100

    Update x.js

--- js/x.js
+++ js/x.js
@@ -25,7 +25,7 @@ images = [
     
 ]
 credits = [
-"Ben Holmes <a href="google.com"> IG @oz_creative_ </a>",
+"Ben Holmes  IG @oz_creative_",
 "Angus Black <p> IG @photography_by_angus_black",
 "Sam Farokh <p> IG @mrphotographer.au",
 "Ben Holmes <p> IG @oz_creative_",
